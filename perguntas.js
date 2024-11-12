8c19effe8b11ec93c91446164e63d1857230c9ba
flavia0865 <160143425+flavia0865@users.noreply.github.com>
2024-11-12 10:39:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Programação',
+    'Qual é a floresta tropical mais extensa do mundo?',
+    'A Floresta Amazônica.'
+)
+
+
+criaCartao(
+    'Programação',
+    'Qual é o país mais populoso do mundo?',
+    'China.'
+)
+
+
+criaCartao(
+    'Programação',
+    'Quais são as cores da bandeira da china?',
+    'Vermelho e amarelos.'
+)
+
+
+criaCartao(
+    'Programação',
+    'Qual é o principal rio da cidade de São Paulo?',
+    'Tiête.'
+)
+
+
+criaCartao(
+    'Programação',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest.'
+)
+
+
+criaCartao(
+    'Programação',
+    'Que país tem o formato de uma bota?',
+    'Itália.'
+)
+
